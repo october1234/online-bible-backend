edd640cc45c24a3237f734413be3311c6aba2200
october <octoberserver@gmail.com>
2023-07-13 17:26:55 +0800

    something

--- app.cpp
+++ app.cpp
@@ -1,6 +1,12 @@
 #include "libs/crow_all.h"
 #include "libs/sqlite-3.42.0/sqlite3.h"
 #include <string>
+#include <regex>
+#include <iostream>
+#include <string>
+#include <codecvt>
+#include <fstream>
+#include "libs/utf8.h"
 
 int done = 0;
 char *zErrMsg = 0;
@@ -19,7 +25,7 @@ int main() {
       .prefix("/nocors")
         .ignore();
 
-    rc = sqlite3_open("test.sqlite", &db);
+    rc = sqlite3_open("Hb5_utf8_rev2.sqlite", &db);
 
     if(rc) {
         fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
@@ -28,29 +34,41 @@ int main() {
         fprintf(stderr, "Opened database successfully\n");
     }
 
-    CROW_ROUTE(app, "/api/get-verses/<int>/<int>")([](int b, int c){
-        std::string res;
-        res = std::to_string(b) + " - " + std::to_string(c);
-        std::cout << res << std::endl;
 
-        std::string s = "";
-        std::string* param = &s;
+    CROW_ROUTE(app, "/api/get-verses/<int>/<int>")([](int book, int chapter){
+        // std::string res;
+        // res = std::to_string(b) + " - " + std::to_string(c);
+        // std::cout << res << std::endl;
 
-        /* Create SQL statement */
-        char* sql = "SELECT * from test";
-        /* Execute SQL statement */
-        rc = sqlite3_exec(db, sql, [](void *param, int argc, char **argv, char **azColName) -> int {
+        // Open a JSON array
+        std::string res = "[";
+
+        // SQL query (No need to worry about SQL injection since parameters are only allowed to be integers)
+        std::string sql = "SELECT * FROM Hb5_utf8_rev1 WHERE Book='" + std::to_string(book) + "' AND Chapter='" + std::to_string(chapter) + "'";
+        // Execute SQL query
+        rc = sqlite3_exec(db, sql.c_str(), [](void *param, int argc, char **argv, char **azColName) -> int {
+            // Get string pointer from param
             std::string* s = (std::string*) param;
+            // Create a string for this row and open the JSON object
+            std::string verse = "{";
 
             int i;
             for(i = 0; i<argc; i++){
                 // sprintf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-                *s = *s + azColName[i] + ": " + (argv[i] ? argv[i] : "NULL") + "\n";
+                // Add entry to the JSON object
+                verse = verse + "\"" + azColName[i] + "\":\"" + (argv[i] ? argv[i] : "NULL") + "\",";
             }
 
-            return 0;
-        }, param, &zErrMsg);
+            // Remove the comma from the last entry
+            verse.pop_back();
+            // Close the JSON object
+            verse += "},";
+            // Add row to the JSON array
+            *s = *s + verse;
 
+            return 0;
+        }, &res, &zErrMsg);
+        // Detect for errors
         if( rc != SQLITE_OK ) {
             fprintf(stderr, "SQL error: %s\n", zErrMsg);
             sqlite3_free(zErrMsg);
@@ -58,8 +76,17 @@ int main() {
             fprintf(stdout, "Operation done successfully\n");
         }
 
-        std::cout << s << std::endl;
-        return s;
+        // Remove the comma from the last object
+        res.pop_back();
+        // Close JSON array
+        res += "]";
+        // Replace all \n characters
+        res = std::regex_replace(res, std::regex("\n"), "\\n");
+
+
+
+        std::string abc = "你好啊！";
+        return crow::response(abc);
     });
 
     app.port(18080).run();
--- test.cpp
+++ test.cpp
@@ -1,12 +1,46 @@
+#include "libs/crow_all.h"
+#include "libs/sqlite-3.42.0/sqlite3.h"
 #include <string>
+#include <regex>
 #include <iostream>
+#include <string>
+#include <codecvt>
+#include <fstream>
+#include "libs/utf8.h"
 
-void abcd(void* a) {
-    std::string s((char*) a);
-    std::cout << s << std::endl;
-}
+int done = 0;
+char *zErrMsg = 0;
+sqlite3 *db;
+int rc;
+std::wofstream myfile;
 
 int main() {
-    char a[] = "hello";
-    abcd(a);
+    rc = sqlite3_open("Hb5_utf8_rev2.sqlite", &db);
+    if(rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return 1;
+    } else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+
+    std::string sql = "SELECT * FROM Hb5_utf8_rev1 WHERE Book='1' AND Chapter='1'";
+    rc = sqlite3_exec(db, sql.c_str(), [](void *param, int argc, char **argv, char **azColName) -> int {
+        int i;
+        for(i = 0; i<argc; i++){
+            std::string s = argv[i] ? argv[i] : "NULL";
+            printf("%s = %s\n", azColName[i], s.c_str());
+        }
+        return 0;
+    }, NULL, &zErrMsg);
+
+    if( rc != SQLITE_OK ) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    } else {
+        fprintf(stdout, "Operation done successfully\n");
+    }
+
+
+    sqlite3_close(db);
+    return 0;
 }
